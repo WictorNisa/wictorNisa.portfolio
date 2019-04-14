672db7eaf2a8b8c09a27e6d6df6c822ec1ecc901
Blue Circus <wictornisa@hotmail.com>
2019-04-14 22:53:38 +0200

    fixed fade in effect

---
+++ js/app.js
@@ -0,0 +1,16 @@
+  // var rellax = new Rellax('.rellax', {
+  //   speed: -2,
+  //   center: false,
+  //   wrapper: null,
+  //   round: true,
+  //   vertical: true,
+  //   horizontal: false
+  // });
+
+const timeHi = document.querySelector('.test');
+const hiddenSpan = document.querySelector('.hidden-h');
+
+// setTimeout(function () {
+//   timeHi.style.transform = "scale(1.3)";
+//   hiddenSpan.style.visibility = 'visible';
+// }, 2000);
