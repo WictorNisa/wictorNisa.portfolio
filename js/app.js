461e34f4a570c42b1b7c49d7497e25299ced0d08
Blue Circus <wictornisa@hotmail.com>
2019-04-15 10:24:46 +0200

    working on about me section

--- js/app.js
+++ js/app.js
@@ -1,16 +1,8 @@
-  // var rellax = new Rellax('.rellax', {
-  //   speed: -2,
-  //   center: false,
-  //   wrapper: null,
-  //   round: true,
-  //   vertical: true,
-  //   horizontal: false
-  // });
-
-const timeHi = document.querySelector('.test');
-const hiddenSpan = document.querySelector('.hidden-h');
-
-// setTimeout(function () {
-//   timeHi.style.transform = "scale(1.3)";
-//   hiddenSpan.style.visibility = 'visible';
-// }, 2000);
+  var rellax = new Rellax('.rellax', {
+    speed: -1,
+    center: false,
+    wrapper: null,
+    round: true,
+    vertical: true,
+    horizontal: false
+  });
