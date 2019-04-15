88d58f90d56e12666e9920b7a30b502d4e41db26
Blue Circus <wictornisa@hotmail.com>
2019-04-15 18:33:31 +0200

    save

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
   var rellax = new Rellax('.rellax', {
-    speed: -1,
+    speed: -1.5,
     center: false,
     wrapper: null,
     round: true,
     vertical: true,
     horizontal: false
   });
+
+
+  const menuScroll = document.querySelectorAll('.menu-wrap .hamburger > div');
+
+console.log(menuScroll);
