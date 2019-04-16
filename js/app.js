c0f7965fe98e34df1a1cf289d2387e2f8f8dd769
Blue Circus <wictornisa@hotmail.com>
2019-04-16 08:42:38 +0200

    working on modal content

--- js/app.js
+++ js/app.js
@@ -1,3 +1,37 @@
+const modal = document.getElementById('simpleModal');
+const modalOpenOne = document.querySelector('.project1');
+
+
+window.addEventListener('click', openModal);
+
+
+function openModal(e){
+  const cloesetDiv1 = e.target.closest('.project1')
+  const cloesetDiv2 = e.target.closest('.project2')
+  if(cloesetDiv1) {
+    console.log('HI');
+    modal.style.display = 'flex';
+  } else if (cloesetDiv2) {
+    console.log('HI this is number 2');
+    modal.style.display = 'flex';
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   var rellax = new Rellax('.rellax', {
     speed: -1.5,
     center: false,
@@ -6,8 +40,3 @@
     vertical: true,
     horizontal: false
   });
-
-
-  const menuScroll = document.querySelectorAll('.menu-wrap .hamburger > div');
-
-console.log(menuScroll);
