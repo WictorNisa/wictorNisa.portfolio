73648e9d49c3ffac896514d5edd0fe2043fd4697
Blue Circus <wictornisa@hotmail.com>
2019-04-17 10:58:40 +0200

    fixed modal and project section

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ function outsideClick(e) {
 }
 
 function closeModal(e) {
-  if(e.target == closeButton){
+  if(e.target.classList.contains('close-modal')){
     modal.style.display = 'none';
   }
 }
@@ -27,31 +27,64 @@ function openModal(e){
   const cloesetDiv3 = e.target.closest('.project3')
   const cloesetDiv4 = e.target.closest('.project4')
   if(cloesetDiv1) {
-    console.log('HI');
     modal.style.display = 'flex';
     let html = `
-    <button class="close-modal">X</button>
+    <button class="close-modal">Close</button>
     <img class="modal-img-view" src="img/project-5-img.png">
     <h2 class="bigH">Photo Gallery</h2>
     <p class="modal-p">A photo gallery project where I had to use CSS grids and the JQuery library and two plugins, one of them being a lightbox plugin.</p>
-    <div>
-    <button class="github-button">Github</button>
-    <button class="liveproject-button">Live Example</button>
+    <div class="modal-buttons">
+    <button class="github-button"><a target="_blank" href="https://github.com/WictorNisa/techdegree-project5">Github &#60 &#47 &#62</a></button>
+    <button class="liveproject-button"><a target="_blank"
+    href="https://agitated-lamport-3acfa8.netlify.com">Live Example</a></button>
     </div>
     `;
     modalContent.innerHTML = html;
     }
     else if (cloesetDiv2) {
-    console.log('HI this is number 2');
     modal.style.display = 'flex';
+    let html = `
+    <button class="close-modal">Close</button>
+    <img class="modal-img-view" src="img/project-6-img.png">
+    <h2 class="bigH">GameShow App </h2>
+    <p class="modal-p">A Gameshow app where I had alot of JavaScript pratice working with arrays and the DOM.</p>
+    <div class="modal-buttons">
+    <button class="github-button"><a target="_blank" href="https://github.com/WictorNisa/Project-6">Github &#60 &#47 &#62</a></button>
+    <button class="liveproject-button"><a target="_blank"
+    href="https://jolly-haibt-d93b1e.netlify.com">Live Example</a></button>
+    </div>
+    `;
+    modalContent.innerHTML = html;
     }
     else if (cloesetDiv3) {
-    console.log('HI this is number 3');
     modal.style.display = 'flex';
+    let html = `
+    <button class="close-modal">Close</button>
+    <img class="modal-img-view" src="img/project-7-img.png">
+    <h2 class="bigH">Web DashBoard</h2>
+    <p class="modal-p">A cool Web dashboard where I had to work with all kinds of tools like, CSS grids, inline SVG and the chart.js library.</p>
+    <div class="modal-buttons">
+    <button class="github-button"><a target="_blank" href="https://github.com/WictorNisa/Project-7">Github &#60 &#47 &#62</a></button>
+    <button class="liveproject-button"><a target="_blank"
+    href="https://boring-lamport-32d775.netlify.com">Live Example</a></button>
+    </div>
+    `;
+    modalContent.innerHTML = html;
     }
     else if (cloesetDiv4) {
-    console.log('HI this is number 4');
     modal.style.display = 'flex';
+    let html = `
+    <button class="close-modal">Close</button>
+    <img class="modal-img-view" src="img/project-8-img.png">
+    <h2 class="bigH">Employee directory</h2>
+    <p class="modal-p">A very tough but fun project where I had to work with APIS. I got practice with the fetch api, promises and displaying random users everytime the user reloads the interface.</p>
+    <div class="modal-buttons">
+    <button class="github-button"><a target="_blank" href="https://github.com/WictorNisa/Project-8">Github &#60 &#47 &#62</a></button>
+    <button class="liveproject-button"><a target="_blank"
+    href="https://dazzling-clarke-7cd055.netlify.com">Live Example</a></button>
+    </div>
+    `;
+    modalContent.innerHTML = html;
     }
   }
 
