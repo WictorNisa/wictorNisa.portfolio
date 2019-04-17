5fd5ce31c70f77276b1c859e42703af372f1fe4a
Blue Circus <wictornisa@hotmail.com>
2019-04-17 10:12:06 +0200

    problem with close button

--- js/app.js
+++ js/app.js
@@ -1,18 +1,56 @@
 const modal = document.getElementById('simpleModal');
 const modalOpenOne = document.querySelector('.project1');
+const modalContent = document.querySelector('.modal-content');
+const closeButton = document.querySelector('.close-modal');
 
 
 window.addEventListener('click', openModal);
+window.addEventListener('click', outsideClick);
+modal.addEventListener('click', closeModal);
 
+
+function outsideClick(e) {
+  if(e.target == modal) {
+    modal.style.display = 'none';
+  }
+}
+
+function closeModal(e) {
+  if(e.target == closeButton){
+    modal.style.display = 'none';
+  }
+}
 
 function openModal(e){
   const cloesetDiv1 = e.target.closest('.project1')
   const cloesetDiv2 = e.target.closest('.project2')
+  const cloesetDiv3 = e.target.closest('.project3')
+  const cloesetDiv4 = e.target.closest('.project4')
   if(cloesetDiv1) {
     console.log('HI');
     modal.style.display = 'flex';
-  } else if (cloesetDiv2) {
+    let html = `
+    <button class="close-modal">X</button>
+    <img class="modal-img-view" src="img/project-5-img.png">
+    <h2 class="bigH">Photo Gallery</h2>
+    <p class="modal-p">A photo gallery project where I had to use CSS grids and the JQuery library and two plugins, one of them being a lightbox plugin.</p>
+    <div>
+    <button class="github-button">Github</button>
+    <button class="liveproject-button">Live Example</button>
+    </div>
+    `;
+    modalContent.innerHTML = html;
+    }
+    else if (cloesetDiv2) {
     console.log('HI this is number 2');
+    modal.style.display = 'flex';
+    }
+    else if (cloesetDiv3) {
+    console.log('HI this is number 3');
+    modal.style.display = 'flex';
+    }
+    else if (cloesetDiv4) {
+    console.log('HI this is number 4');
     modal.style.display = 'flex';
     }
   }
